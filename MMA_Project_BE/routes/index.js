1dd7351edf89badb4d21d97beb4b4c4c1ba616fc
hwn-0704 <hosyhung419@gmail.com>
2025-11-10 23:56:59 +0700

    Delete MMA_Project_BE directory

--- MMA_Project_BE/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authRouter = require("./authRouter");
-const productRouter = require("./productRouter");
-
-router.use("/auth", authRouter);
-router.use("/product", productRouter);
-
-module.exports = router;
